8075557142afb9cacf6f74aac27f454e838cdc5a
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-05-10 12:37:30 +0200

    aggiunto risultato (giusto) per es.10

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -157,3 +157,95 @@ for (let index = 0; index < charactersArray.length; index++) {
 }
 
 console.log(positionsArray);
+
+const charactersArray2 = ["g", "n", "u", "z", "d"];
+const positionsArray2 = [];
+
+for (let index = 0; index < charactersArray.length; index++) {
+  let position = 0;
+  switch (charactersArray[index]) {
+    case "a":
+      position = 1;
+      break;
+    case "b":
+      position = 2;
+      break;
+    case "c":
+      position = 3;
+      break;
+    case "d":
+      position = 4;
+      break;
+    case "e":
+      position = 5;
+      break;
+    case "f":
+      position = 6;
+      break;
+    case "g":
+      position = 7;
+      break;
+    case "h":
+      position = 8;
+      break;
+    case "i":
+      position = 9;
+      break;
+    case "j":
+      position = 10;
+      break;
+    case "k":
+      position = 11;
+      break;
+    case "l":
+      position = 12;
+      break;
+    case "m":
+      position = 13;
+      break;
+    case "n":
+      position = 14;
+      break;
+    case "o":
+      position = 15;
+      break;
+    case "p":
+      position = 16;
+      break;
+    case "q":
+      position = 17;
+      break;
+    case "r":
+      position = 18;
+      break;
+    case "s":
+      position = 19;
+      break;
+    case "t":
+      position = 20;
+      break;
+    case "u":
+      position = 21;
+      break;
+    case "v":
+      position = 22;
+      break;
+    case "w":
+      position = 23;
+      break;
+    case "x":
+      position = 24;
+      break;
+    case "y":
+      position = 25;
+      break;
+    case "z":
+      position = 26;
+      break;
+    default:
+      position = 0;
+  }
+  positionsArray2.push(position);
+}
+
+console.log(positionsArray2);
